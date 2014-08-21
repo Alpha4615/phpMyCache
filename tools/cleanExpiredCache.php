9834ab36cfab890c0b9b2adbe3161a1ca24db8c9
Alpha4615 <jknipp@gmail.com>
2014-08-21 06:10:12 +0000

    Updating tool to support JSON format Resolves #13

--- tools/cleanExpiredCache.php
+++ tools/cleanExpiredCache.php
@@ -10,7 +10,7 @@ foreach ($scan as $key => $val) {
     if ($val != '..' && $val != '.' && is_dir($val) == FALSE) {
         $val       = $cacheDirectory . $val;
         $contents  = file_get_contents($val);
-        $cacheData = unserialize($contents);
+        $cacheData = json_decode($contents, TRUE);
         $date      = "m/d/y H:i:s";
         echo date($date) . " -- " . date($date, $cacheData['createdDate'] + $cacheData['expires']) . PHP_EOL;
 
